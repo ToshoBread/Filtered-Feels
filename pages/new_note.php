ab9180033ef9bebaaeae39c66dc5d95e5ed46beb
ToshoBread <93123377+ToshoBread@users.noreply.github.com>
2025-08-27 15:09:47 +0800

    feat(form): Image header for notes feat(form): Note signature feat: Client-side form validation

--- pages/new_note.php
+++ pages/new_note.php
@@ -4,49 +4,81 @@
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Filtered Feels: New Note</title>
-        <link
-            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css"
-            rel="stylesheet"
-            integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr"
-            crossorigin="anonymous"
-        />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous"/>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
         <style>
-        img[src=""] {
-            display: none
-        }
+        img[src=""] {display: none}
+        input[type="file"]::file-selector-button {display: none}
+        label {cursor: pointer; user-select: none;}
         </style>
     </head>
-    <body class="bg-secondary-subtle" style="height: 100vh">
+    <body class="bg-secondary-subtle" style="min-height: 100vh;">
         <?php require '../components/navbar.html'?>
         <form action="../services/new_note.php" method="post" class="container mt-5">
-            <div class="mb-3 form-floating border border-secondary rounded-1">
-                <input type="text" class="form-control" id="title" placeholder="" />
-                <label for="title" class="form-label">Title</label>
+            <div class="mb-3 input-group-text border border-secondary rounded-1">
+                <label for="title">Title</label>
+                <input
+                    type="text"
+                    minlength="3"
+                    maxlength="5"
+                    class="form-control mx-3 border border-secondary px-1"
+                    id="title"
+                    aria-required="true"
+                    aria-label="Input Title"
+                    required
+                />
+                <label for="signature">Signature</label>
+                <input
+                    type="text"
+                    maxlength="30"
+                    class="form-control mx-3 border border-secondary px-1"
+                    id="signature"
+                    placeholder="Someone"
+                    value="Someone"
+                    aria-required="false"
+                    aria-label="Input Signature"
+                />
             </div>
             <div class="mb-3 input-group-text border border-secondary">
-                <label for="formFile">Header Image</label>
+                <label for="input-header-img">Header Image</label>
                 <input
                     class="form-control border-secondary mx-2"
                     type="file"
                     accept="image/*"
-                    id="header-img"
+                    id="input-header-img"
+                    aria-label="Input Header Image"
                 />
             </div>
-            <img src="" id="img-preview" class="rounded-top w-100" style="height: 20vh; object-fit: cover;" />
+            <div class="input-group overflow-hidden w-100">
+                <img
+                    src=""
+                    id="img-preview"
+                    class="w-100 h-100"
+                    style="aspect-ratio: 4/1; object-fit: cover; object-position: center;"
+                    aria-label="Image Preview"
+                />
+                <div class="card-img-overlay m-2">
+                    <button
+                        type="button"
+                        id="remove-img-btn"
+                        class="btn btn-outline-danger float-end"
+                        aria-label="Close Image Preview"
+                    ><i class="bi bi-x-lg fs-6"></i></button>
+                </div>
+            </div>
             <textarea
-                class="form-control border border-secondary rounded-1"
+                placeholder="Write here..."
+                minlength="3"
                 id="note-content"
+                class="form-control border border-secondary rounded-1"
                 style="height: 25rem; resize: none;"
+                aria-required="true"
+                required
             ></textarea>
-            <a href="test.php" class="btn btn-danger mt-2 px-5">Discard</a>
-            <input type="submit" id="submit" class="btn btn-success mt-2 px-5 float-end" />
+            <input type="reset" value="Reset" id="discard-btn" class="btn btn-danger mt-2 px-5" aria-label="Reset">
+            <input type="submit" value="Post" id="submit-btn" class="btn btn-success mt-2 px-5 float-end" aria-label="Submit" />
         </form>
-        <script src="../scripts/new_note.js">
-        </script>
-        <script
-        src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q"
-        crossorigin="anonymous"
-    ></script>
+        <script src="../scripts/new_note.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
     </body>
 </html>
--- scripts/new_note.js
+++ scripts/new_note.js
@@ -1,6 +1,9 @@
-const imgInput = document.getElementById("header-img");
+const imgInput = document.getElementById("input-header-img");
 const imgPreview = document.getElementById("img-preview");
 const noteContent = document.getElementById("note-content");
+
+const removeImgBtn = document.getElementById("remove-img-btn");
+removeImgBtn.style.display = "none";
 
 imgInput.onchange = () => {
   const [file] = imgInput.files;
@@ -9,5 +12,15 @@ imgInput.onchange = () => {
     noteContent.classList.remove("rounded-1");
     noteContent.classList.add("rounded-top-0");
     noteContent.classList.add("rounded-bottom-1");
+    removeImgBtn.style.display = "block";
   }
+};
+
+removeImgBtn.onclick = () => {
+  imgInput.value = "";
+  imgPreview.src = "";
+  noteContent.classList.add("rounded-1");
+  noteContent.classList.remove("rounded-top-0");
+  noteContent.classList.remove("rounded-bottom-1");
+  removeImgBtn.style.display = "none";
 };
