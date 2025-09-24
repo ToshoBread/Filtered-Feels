04ee388d37a74a56e16f3802255867e533e1476c
ToshoBread <93123377+ToshoBread@users.noreply.github.com>
2025-09-24 17:46:11 +0800

    add: post validation add(WIP): PDO, Post, and User methods change: header image will now be VARCHAR instead of BLOB

---
+++ db/Post.php
@@ -0,0 +1,34 @@
+<?php
+
+abstract class Post
+{
+    public static function addPost(string $title, string $content, string $signature, ?string $header_image = null)
+    {
+        $sql = 'INSERT INTO Post (title, content, header_image, signature)
+                VALUES (:title, :content, :header_image, :signature)';
+        $values = [
+            ':title' => $title,
+            ':content' => $content,
+            ':header_image' => $header_image,
+            ':signature' => $signature,
+        ];
+
+        Db::query($sql, $values);
+    }
+
+    public static function getPostByUserId(int $user_id)
+    {
+        $sql = 'SELECT * FROM Post WHERE post_id = :post_id';
+        $value = [':user_id' => $user_id];
+
+        return Db::selectOne($sql, $value);
+    }
+
+    public static function getPostByPostId(int $post_id)
+    {
+        $sql = 'SELECT * FROM Post WHERE post_id = :post_id';
+        $value = [':post_id' => $post_id];
+
+        return Db::selectOne($sql, $value);
+    }
+}
---
+++ db/User.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once 'db.php';
+
+abstract class User
+{
+    public static function getUser(string $username)
+    {
+        $sql = 'SELECT * FROM User WHERE username = :username';
+        $value = [':username' => $username];
+
+        return Db::selectOne($sql, $value);
+    }
+}
