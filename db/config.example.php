bbd0d6b03ca12eff3d692a5342b0cbd05bbcab76
ToshoBread <93123377+ToshoBread@users.noreply.github.com>
2025-10-08 02:52:05 +0800

    chore: new example config add: automate deployment

---
+++ db/config.example.php
@@ -0,0 +1,14 @@
+<?php
+
+abstract class Config
+{
+    public static $DB_HOSTNAME = 'DB_HOSTNAME';
+
+    public static $DB_USERNAME = 'DB_USERNAME';
+
+    public static $DB_PASSWORD = 'DB_PASSWORD';
+
+    public static $DB_NAME = 'DB_NAME';
+
+    public static $DB_PORT = 0;
+}
