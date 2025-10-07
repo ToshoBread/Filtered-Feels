442840f199080210c4643f6b40cba13226d961cc
ToshoBread <93123377+ToshoBread@users.noreply.github.com>
2025-10-08 03:24:41 +0800

    fix: accidentally pasted to post.php

--- post.php
+++ post.php
@@ -2,92 +2,80 @@
 session_start();
 $_SESSION['prev_page'] = $_SERVER['REQUEST_URI'];
 
-if (! empty($_SESSION['username'])) {
-    $username = $_SESSION['username'];
+require_once 'db/Post.php';
+require_once 'db/User.php';
+require_once 'services/util.php';
+require_once 'components/navbar.php';
+
+$postId = (int) $_GET['post'];
+$userId = (int) $_GET['user'];
+
+$post = Post::selectPostByPostId($postId);
+$user = User::selectUserById($userId);
+
+try {
+    if (empty($postId)) {
+        throw new Exception('Post ID is missing');
+    }
+
+    if (empty($post)) {
+        throw new Exception('Post does not exist');
+    }
+
+    if ($userId != 0 && empty($user)) {
+        throw new Exception('User does not exist');
+    }
+
+    if (($userId !== $post['user_id'] && $userId !== 0) || ($userId === 0 && $post['user_id'] !== null)) {
+        throw new Exception('User to Post mismatch');
+    }
+} catch (Exception $e) {
+    addToErrLog('Post Display Error', $e->getMessage());
+    header('Location: index.php');
+    exit();
 }
 
-require_once 'components/navbar.php';
-require_once 'services/util.php';
+$headerImage = trim($post['header_image']);
+$borderColor = trim($post['border_color']);
+$title = trim($post['title']);
+$signature = trim($post['signature']);
+$content = trim($post['content']);
+$createdOn = strtok($post['created_on'], ' ');
 
 ?>
 <!doctype html>
 <html lang="en">
     <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Filtered Feels: New Post</title>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Filtered Feels: <?= $post['title']?></title>
         <link rel="icon" type="image/x-icon" href="assets/fav_logo.svg">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous"/>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
         <link rel="stylesheet" href="styles/base.css">
+        <style>
+        img:not([src=""]){aspect-ratio: 4/2; object-position: center;}
+        img[src=""], img[src="#"] {display: none;}
+        input[type="file"]::file-selector-button {display: none}
+        </style>
     </head>
     <body>
         <?= Navbar()?>
-        <form action="services/new_post_validation.php"
-            method="post" enctype="multipart/form-data"
-            id="new-post-form"
-            class="container-md mt-5 rounded-2 p-4">
-            <div class="form-floating my-3">
-                <input
-                    type="text"
-                    minlength="3"
-                    maxlength="50"
-                    class="form-control border fs-5"
-                    id="title"
-                    name="title"
-                    placeholder=""
-                    aria-required="true"
-                    aria-label="Input Title"
-                    required
-                />
-                <label for="title">Title</label>
-            </div>
-            <div class="form-floating my-3">
-                <input
-                    type="text"
-                    maxlength="30"
-                    class="form-control border fs-5"
-                    id="signature"
-                    name="signature"
-                    placeholder="Someone"
-                    value=<?= isset($username) ? $username : 'Someone'?>
-                    aria-required="false"
-                    aria-label="Input Signature"
-                />
-                <label for="signature">Signature</label>
-            </div>
-            <h6 class="text-center text-light" style="user-select: none;">Border Color:</h6>
-            <div class="d-flex gap-3 flex-wrap justify-content-center mb-3">
-                <?php
-                foreach (Colors::cases() as $color) { ?>
-                <label>
-                    <input type="radio" value="<?= colorToHex($color)?>" tabindex="-1" name="color" class="d-none"
-                        <?= colorToHex($color) === 'FFFFFF' ? 'checked' : ''?>
-                    />
-                    <span class="color-circle rounded-circle"
-                        style="background: #<?= colorToHex($color)?>;"></span>
-                </label>
-                <?php }?>
-            </div>
-            <label class="btn border mb-3 col col-4 offset-4" 
-                for="input-header-img">Upload Header Image</label>
-            <input
-                type="file"
-                accept="image/*"
-                id="input-header-img"
-                name="header-img"
-                class="d-none"
-                aria-label="Input Header Image"
-            />
-            <div class="input-group overflow-hidden w-100">
+        <input type="hidden" form="edit-post-form" name="post-id" value="<?= htmlspecialchars($postId) ?>">
+        <input type="hidden" form="edit-post-form" name="user-id" value="<?= htmlspecialchars($userId) ?>">
+        <input type="hidden" form="edit-post-form" id="deleted-img-flag" name="deleted-img-flag" value="0">
+        <input type="hidden" form="edit-post-form" name="curr-header-img" value="<?= $headerImage ? htmlspecialchars($headerImage) : null?>">
+
+        <div id="post-container" class="card container-lg p-0 pb-2 rounded-2 text-light"
+            style="border: solid 0.15rem #<?= $borderColor ?>; background: rgb(238, 238, 238, 0);" >
+
+            <div id="header-img-wrapper" class="input-group overflow-hidden w-100">
                 <img
-                    src=""
-                    id="img-preview"
-                    class="w-100 h-100 object-fit-cover border-top"
-                    style="aspect-ratio: 4/2; object-position: center;"
-                    aria-label="Image Preview"
+                    src="<?= $headerImage ? getImage($headerImage) : null?>"
+                    class="card-img-top rounded-0 object-fit-cover"
+                    id="header-img"
                 />
-                <div class="card-img-overlay m-2">
+                <div class="post-input card-img-overlay m-2">
                     <button
                         type="button"
                         id="remove-img-btn"
@@ -96,19 +84,133 @@ require_once 'services/util.php';
                     ><i class="bi bi-x-lg fs-6"></i></button>
                 </div>
             </div>
-            <textarea
-                placeholder="Write here..."
-                minlength="3"
-                id="post-content"
-                name="post-content"
-                class="border rounded-1 mb-3 w-100 p-2"
-                style="min-height: 25rem; resize: none; field-sizing: content;"
-                aria-required="true"
-                required
-            ></textarea>
-            <input type="submit" value="Post" id="submit-btn" name="submit" class="btn btn-outline-success px-5 fs-5 col col-12" aria-label="Submit" />
-        </form>
+
+
+            <div class="card-body">
+                <form action="../services/edit_post_validation.php" method="post" id="edit-post-form" enctype="multipart/form-data"></form>
+
+                <?php if ((isset($_SESSION['user_id']) && $_SESSION['user_id'] === $post['user_id']) || (isset($_SESSION['role']) && $_SESSION['role'] === 'Admin')) { ?>
+
+                <button type="button" id="edit-post-btn" class="post-detail btn btn-outline-light border-0 float-end"><i class="bi bi-pencil-square"></i></button>
+
+                <div class="d-flex gap-2 flex-wrap justify-content-evenly mb-2">
+
+                    <label for="edit-header-img" class="post-input btn btn-secondary">Upload New Image</label>
+                    <input type="file"
+                        form="edit-post-form"
+                        accept="image/*"
+                        id="edit-header-img"
+                        name="edit-header-img"
+                        class="d-none"
+                        style="cursor: pointer; user-select: none;"
+                    />
+
+                    <button form="edit-post-form" type="button" class="post-input btn btn-outline-danger px-4" data-bs-toggle="modal" data-bs-target="#confirm-delete">Delete Post</button>
+                    <button form="edit-post-form" type="button" class="post-input btn btn-outline-success px-4" data-bs-toggle="modal" data-bs-target="#confirm-save">Save</button>
+                    <button type="button" id="cancel-edit-btn" class="post-input btn btn-outline-danger px-4">Cancel</button>
+                </div>
+
+                <h6 class="post-input text-center text-light mt-4" style="user-select: none;">Border Color:</h6>
+                <div class="post-input d-flex gap-3 flex-wrap justify-content-center mt-2 mb-4">
+                    <?php
+                    foreach (Colors::cases() as $color) { ?>
+                    <label>
+                        <input form="edit-post-form" type="radio" value="<?= colorToHex($color)?>" tabindex="-1" name="color" class="d-none"
+                            <?= colorToHex($color) === $borderColor ? 'checked' : ''?>
+                        />
+                        <span class="color-circle rounded-circle"
+                            style="background: #<?= colorToHex($color)?>;"></span>
+                    </label>
+                    <?php }?>
+                </div>
+
+                <input
+                    form="edit-post-form"
+                    type="text" 
+                    id="edit-title"
+                    name="edit-title"
+                    placeholder="<?= $title?>"
+                    value="<?= $title?>"
+                    minlength="3"
+                    maxlength="50"
+                    class="post-input form-control fw-bolder fs-1 mb-3"
+                    aria-required="true"
+                    required
+                />
+
+                <div class="post-input">
+                    <label for="edit-signature" class="form-label fw-semibold fs-5">by</label>
+                    <input 
+                        form="edit-post-form"
+                        type="text"
+                        id="edit-signature"
+                        name="edit-signature"
+                        placeholder="<?= $signature?>"
+                        value="<?= $signature?>"
+                        class="form-control fw-semibold fs-5"
+                        aria-required="true"
+                        required
+                    />
+                </div>
+                <p class="post-input card-text text-secondary"><?= 'Posted on: '.$createdOn?></p>
+                <hr class="post-input"/>
+                <textarea
+                    form="edit-post-form"
+                    id="edit-content-area"
+                    name="edit-content-area"
+                    class="post-input form-control overflow-hidden"
+                    placeholder="<?= $content?>"
+                    aria-required="true"
+                    required
+                ><?= $content?></textarea>
+                <?php }?>
+
+                <p id="title" class="post-detail card-title fs-1 fw-bolder"><?= $title?></p>
+                <p class="post-detail fs-5 fw-semibold">by <span id="signature"><?= $signature?></span></p>
+                <p class="post-detail card-text"><?= 'Posted on: '.$createdOn?></p>
+                <hr class="post-detail"/>
+                <p id="content" class="post-detail card-text fs-5">
+                    <?= $content?>
+                </p>
+            </div>
+        </div>
+        <!--Modals-->
+        <div id="confirm-save" class="modal fade" tabindex="-1">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Confirm</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <p>Are you sure you want to save changes?</p>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button form="edit-post-form" type="submit" name="submit" value="save" class="btn btn-outline-success">Save Changes</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div id="confirm-delete" class="modal fade" tabindex="-1">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Confirm</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <p>Are you sure you want to delete this post?</p>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button form="edit-post-form" type="submit" name="submit" value="delete" class="btn btn-danger">Delete Post</button>
+                    </div>
+                </div>
+            </div>
+        </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
-        <script src="scripts/new_post.js"></script>
+        <script src="../scripts/post.js"></script>
     </body>
 </html>
